40232953d755e953e815e8059a1a78249400b32f
Adriano de Mello Rodrigues <adriano.rodrigues@celk.net>
2020-08-02 00:52:14 -0300

    Binary tree height

---
+++ src/main/java/br/com/adrianorodrigues/algoritms/BinaryTreeHeight.java
@@ -0,0 +1,62 @@
+package br.com.adrianorodrigues.algoritms;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class BinaryTreeHeight {
+
+    public static int height(Node root) {
+        return root == null ? 0 : getMaxHeight(root) - 1;
+    }
+
+    private static int getMaxHeight(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(getMaxHeight(root.left), getMaxHeight(root.right)) + 1;
+    }
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File(System.getProperty("user.dir") + "/src/main/resources/BinaryTreeHeightData.txt");
+        Scanner scan = new Scanner(file);
+        int t = scan.nextInt();
+        Node root = null;
+        while (t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        int height = height(root);
+        System.out.println(height);
+    }
+}
+
